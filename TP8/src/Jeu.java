1d53f35e6223f79216c1fb82d9826ef329389995
Thibault Vlacich <thibault.vlacich@gmail.com>
2016-04-09 14:15:15 +0200

    Unifie l'indentation

--- TP8/src/Jeu.java
+++ TP8/src/Jeu.java
@@ -6,192 +6,192 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class Jeu {
-	public static final int FPS = 5;
-	
-	/**
-	 * Permet d'afficher la grille en mode console
-	 * 
-	 * @param grille la grille à afficher
-	 */
-	public static void afficheConsole(boolean[][] grille) {
-		int m = grille.length;
-		int n = grille[0].length;
-		
-		for(int i = 0; i < m; i++) {
-			System.out.print("|");
-			for(int j = 0; j < n; j++) {
-				if(grille[i][j] == true) {
-					System.out.print("X");
-				} else {
-					System.out.print(" ");
-				}
-				System.out.print("|");
-			}
-			System.out.println("");
-		}
-	}
-	
-	/**
-	 * Permet d'afficher la grille en 2D
-	 * 
-	 * @param grille la grille à afficher
-	 */
-	public static void affiche2D(boolean [][] grille) {
-		int m = grille.length;
-		int n = grille[0].length;
-		
-		StdDraw.setXscale(-1, n);
-		StdDraw.setYscale(-1, m);
-		
-		StdDraw.clear();
-		StdDraw.line(-0.5, m-0.5, n-0.5, m-0.5);
-		StdDraw.line(n-0.5, -0.5, n-0.5, m-0.5);
+  public static final int FPS = 5;
 
-		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < n; j++) {
-				StdDraw.line(j-0.5, i-0.5, j-0.5, i+0.5);
-				StdDraw.line(j-0.5, i-0.5, j+0.5, i-0.5);
-				if(grille[i][j] == true) {
-					StdDraw.setPenColor(StdDraw.BLACK);
-					StdDraw.filledCircle(j, i, 0.5);
-				}
-			}
-		}
-	}
-	
-	/**
-	 * Permet de calculer le nombre de cellules vivantes autour
-	 * d'une cellule donnée
-	 * 
-	 * @param grille la grille du jeu
-	 * @param i la colonne de la cellule à vérifier
-	 * @param j la ligne de la cellule à vérifier
-	 * @return le nombre de cellules vivantes autour de la cellule donnée
-	 */
-	public static int comptage(boolean[][] grille, int i, int j) {
-		int m = grille.length;
-		int n = grille[0].length;
-		
-	    int k = 0;
-	    
-	    for(int p = i-1; p <= i+1; p++) {
-	    	for(int q = j-1; q <= j+1; q++) {
-	    		if(p >= 0 && q >= 0 && p <= n - 1 && q <= m - 1) {
-	    			if(grille[q][p] == true && !(p == i && q == j)) {
-	    				k++;
-	    			}
-	    		}
-	    	}
-	    }
+  /**
+   * Permet d'afficher la grille en mode console
+   *
+   * @param grille la grille à afficher
+   */
+  public static void afficheConsole(boolean[][] grille) {
+    int m = grille.length;
+    int n = grille[0].length;
 
-	    return k;
-	}
-	
-	/**
-	 * Permet de calculer l'état suivant de la grille
-	 * 
-	 * @param grille l'état actuel de la grille
-	 * @return le nouvel état de la grille
-	 */
-	public static boolean[][] step(boolean[][] grille) {
-		int m = grille.length;
-		int n = grille[0].length;
-		
-		boolean[][] grilleNext = new boolean[m][n];
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				int compte = comptage(grille, i, j);
-				
-				if(compte == 3) {
-					grilleNext[j][i] = true;
-				} else if (grille[j][i] == true && compte == 2) {
-					grilleNext[j][i] = true;
-				} else {
-					grilleNext[j][i] = false;
-				}
-			}
-		}
-		
-		return grilleNext;
-	}
-	
-	/**
-	 * Lire un fichier de sauvegarde
-	 * 
-	 * @param fichier le nom du fichier à lire
-	 * @return la grille lue à partir du fichier, null si le fichier n'existe pas
-	 */
-	public static boolean[][] read(String fichier) {
-			String line = null;
-			
-		    ArrayList<boolean[]> list = new ArrayList<boolean[]>();
-		    
-		    try {
-		        BufferedReader reader = new BufferedReader(new FileReader(fichier));
-		        
-		        while((line = reader.readLine()) != null) {
-		            line = line.replace(";", "");
-		            
-		            boolean[] lineB = new boolean[line.length()];
-		            
-		            for(int i = 0; i < line.length(); i++) {
-		            	boolean val = false;
-		            	
-		            	if (line.charAt(i) == '1'){
-		            		val = true;
-		            	}
-		            	
-		            	lineB[i] = val;
-		            }
+    for(int i = 0; i < m; i++) {
+      System.out.print("|");
+      for(int j = 0; j < n; j++) {
+        if(grille[i][j] == true) {
+          System.out.print("X");
+        } else {
+          System.out.print(" ");
+        }
+        System.out.print("|");
+      }
+      System.out.println("");
+    }
+  }
 
-		            list.add(lineB);
-		        }
-		        
-		        reader.close();
-		    } catch (Exception e) {
-		        return null;
-		    }
+  /**
+   * Permet d'afficher la grille en 2D
+   *
+   * @param grille la grille à afficher
+   */
+  public static void affiche2D(boolean [][] grille) {
+    int m = grille.length;
+    int n = grille[0].length;
 
-		    boolean[][] convList = new boolean[list.size()][list.get(0).length];
+    StdDraw.setXscale(-1, n);
+    StdDraw.setYscale(-1, m);
 
-		    for(int n = 0; n < list.size(); n++) {
-		         convList[n] = list.get(n); 
-		    }
-		    
-		    return convList;
-	}
-	
-	/**
-	 * Permet de sauvegarder la grille dans un fichier
-	 * 
-	 * @param grille la grille à sauvegarder
-	 * @param fileName le nom du fichier à écrire
-	 */
-	public static void save(boolean[][] grille, String fileName) {
-		StringBuffer ligne = new StringBuffer();
-		
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(fileName,"UTF-8");
-		
-			for (boolean[] line : grille) {
-				for(boolean tile : line) {
-					String val = tile ? "1" : "0";
-					ligne.append(val + ";");
-				}
-				
-				writer.println(ligne);
-				ligne.delete(0, ligne.length());
-			}
-			
-			writer.close();
-			
-			System.out.println("Grille Sauvegardée");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
+    StdDraw.clear();
+    StdDraw.line(-0.5, m-0.5, n-0.5, m-0.5);
+    StdDraw.line(n-0.5, -0.5, n-0.5, m-0.5);
+
+    for(int i = 0; i < m; i++) {
+      for(int j = 0; j < n; j++) {
+        StdDraw.line(j-0.5, i-0.5, j-0.5, i+0.5);
+        StdDraw.line(j-0.5, i-0.5, j+0.5, i-0.5);
+        if(grille[i][j] == true) {
+          StdDraw.setPenColor(StdDraw.BLACK);
+          StdDraw.filledCircle(j, i, 0.5);
+        }
+      }
+    }
+  }
+
+  /**
+   * Permet de calculer le nombre de cellules vivantes autour
+   * d'une cellule donnée
+   *
+   * @param grille la grille du jeu
+   * @param i la colonne de la cellule à vérifier
+   * @param j la ligne de la cellule à vérifier
+   * @return le nombre de cellules vivantes autour de la cellule donnée
+   */
+  public static int comptage(boolean[][] grille, int i, int j) {
+    int m = grille.length;
+    int n = grille[0].length;
+
+      int k = 0;
+
+      for(int p = i-1; p <= i+1; p++) {
+        for(int q = j-1; q <= j+1; q++) {
+          if(p >= 0 && q >= 0 && p <= n - 1 && q <= m - 1) {
+            if(grille[q][p] == true && !(p == i && q == j)) {
+              k++;
+            }
+          }
+        }
+      }
+
+      return k;
+  }
+
+  /**
+   * Permet de calculer l'état suivant de la grille
+   *
+   * @param grille l'état actuel de la grille
+   * @return le nouvel état de la grille
+   */
+  public static boolean[][] step(boolean[][] grille) {
+    int m = grille.length;
+    int n = grille[0].length;
+
+    boolean[][] grilleNext = new boolean[m][n];
+
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        int compte = comptage(grille, i, j);
+
+        if(compte == 3) {
+          grilleNext[j][i] = true;
+        } else if (grille[j][i] == true && compte == 2) {
+          grilleNext[j][i] = true;
+        } else {
+          grilleNext[j][i] = false;
+        }
+      }
+    }
+
+    return grilleNext;
+  }
+
+  /**
+   * Lire un fichier de sauvegarde
+   *
+   * @param fichier le nom du fichier à lire
+   * @return la grille lue à partir du fichier, null si le fichier n'existe pas
+   */
+  public static boolean[][] read(String fichier) {
+      String line = null;
+
+        ArrayList<boolean[]> list = new ArrayList<boolean[]>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fichier));
+
+            while((line = reader.readLine()) != null) {
+                line = line.replace(";", "");
+
+                boolean[] lineB = new boolean[line.length()];
+
+                for(int i = 0; i < line.length(); i++) {
+                  boolean val = false;
+
+                  if (line.charAt(i) == '1'){
+                    val = true;
+                  }
+
+                  lineB[i] = val;
+                }
+
+                list.add(lineB);
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            return null;
+        }
+
+        boolean[][] convList = new boolean[list.size()][list.get(0).length];
+
+        for(int n = 0; n < list.size(); n++) {
+             convList[n] = list.get(n);
+        }
+
+        return convList;
+  }
+
+  /**
+   * Permet de sauvegarder la grille dans un fichier
+   *
+   * @param grille la grille à sauvegarder
+   * @param fileName le nom du fichier à écrire
+   */
+  public static void save(boolean[][] grille, String fileName) {
+    StringBuffer ligne = new StringBuffer();
+
+    PrintWriter writer;
+    try {
+      writer = new PrintWriter(fileName,"UTF-8");
+
+      for (boolean[] line : grille) {
+        for(boolean tile : line) {
+          String val = tile ? "1" : "0";
+          ligne.append(val + ";");
+        }
+
+        writer.println(ligne);
+        ligne.delete(0, ligne.length());
+      }
+
+      writer.close();
+
+      System.out.println("Grille Sauvegardée");
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- TP8/src/TP8.java
+++ TP8/src/TP8.java
@@ -2,78 +2,73 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class TP8 {
-	
-	static int nLetterKeycode = 78;
-	static int sLetterKeycode = 83;
-	
-	static boolean [][] defaultGrid = {
-			{false, false, true, false},
-			{true,  true,  true, false},
-			{false, true, false, false}		
-		};
 
-	public static void main(String [] args) throws IOException {
-		if(args.length > 0 && args[0].equals("console")) {
-		    handleConsole();
-		}
-		else {
-			handle2D();
-		}
-	 
-	}
-	
-	/**
-	 * Permet de gérer l'affichage en 2D du jeu
-	 */
-	private static void handle2D() {
-		
-		boolean[][] grille = defaultGrid;
-		
-		boolean[][] savedGrille = Jeu.read("save.txt");
-		
-		if(savedGrille != null) {
-			grille = savedGrille;
-			System.out.println("Grille lue depuis le fichier de sauvegarde");
-		}
-		
-		while(true) {
-			if (StdDraw.hasNextKeyTyped()) {
-				if(StdDraw.isKeyPressed(nLetterKeycode)) {
-					// Appui sur la touche n
-					// Permet de passer à l'étape suivante du jeu
-					grille = Jeu.step(grille);
-				}
-				
-				if(StdDraw.isKeyPressed(sLetterKeycode)) {
-					// Appui sur la touche s
-					// Permet de sauvegarder l'état actuel du jeu
-					Jeu.save(grille, "save.txt");
-				}
-			}
-			
-			StdDraw.clear();
-			Jeu.affiche2D(grille);
-			StdDraw.show(1000/Jeu.FPS);
-		}
-	}
+  static int nLetterKeycode = 78;
+  static int sLetterKeycode = 83;
 
-	/**
-	 * Permet de gérer l'affichage en mode console du jeu
-	 */
-	public static void handleConsole() {
-		boolean[][] grille = defaultGrid;
-		
-		String ch = "a";
-		Scanner scan = new Scanner(System.in);
-		
-		while(!ch.equals("q")) { // On quitte en appuyant sur "q" puis entrer
-			Jeu.afficheConsole(grille);
-			ch = scan.nextLine();
-			grille = Jeu.step(grille);
-		}
-		
-		scan.close();
-		
-	}
+  static boolean [][] defaultGrid = {
+    {false, false, true, false},
+    {true,  true,  true, false},
+    {false, true, false, false}
+  };
 
+  public static void main(String [] args) throws IOException {
+    if(args.length > 0 && args[0].equals("console")) {
+        handleConsole();
+    } else {
+      handle2D();
+    }
+  }
+
+  /**
+   * Permet de gérer l'affichage en 2D du jeu
+   */
+  private static void handle2D() {
+    boolean[][] grille = defaultGrid;
+
+    boolean[][] savedGrille = Jeu.read("save.txt");
+
+    if(savedGrille != null) {
+      grille = savedGrille;
+      System.out.println("Grille lue depuis le fichier de sauvegarde");
+    }
+
+    while(true) {
+      if (StdDraw.hasNextKeyTyped()) {
+        if(StdDraw.isKeyPressed(nLetterKeycode)) {
+          // Appui sur la touche n
+          // Permet de passer à l'étape suivante du jeu
+          grille = Jeu.step(grille);
+        }
+
+        if(StdDraw.isKeyPressed(sLetterKeycode)) {
+          // Appui sur la touche s
+          // Permet de sauvegarder l'état actuel du jeu
+          Jeu.save(grille, "save.txt");
+        }
+      }
+
+      StdDraw.clear();
+      Jeu.affiche2D(grille);
+      StdDraw.show(1000/Jeu.FPS);
+    }
+  }
+
+  /**
+   * Permet de gérer l'affichage en mode console du jeu
+   */
+  public static void handleConsole() {
+    boolean[][] grille = defaultGrid;
+
+    String ch = "a";
+    Scanner scan = new Scanner(System.in);
+
+    while(!ch.equals("q")) { // On quitte en appuyant sur "q" puis entrer
+      Jeu.afficheConsole(grille);
+      ch = scan.nextLine();
+      grille = Jeu.step(grille);
+    }
+
+    scan.close();
+  }
 }
